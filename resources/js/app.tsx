fc0cff513804dfb4e90508ea028865d0c653319c
Martin Saldinger <martin.saldinger@gmail.com>
2024-11-24 11:19:44 +0100

    axios inerceptors

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,22 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
+
+import {default as Axios} from 'axios';
+
+Axios.interceptors.request.use((res) => res, function (error) {
+    return Promise.reject({
+        "hello": "I am not an axios error"
+    });
+})
+
+Axios.interceptors.response.use((res) => res, function (error) {
+    return Promise.reject({
+        "hello": "I am not an axios error"
+    });
+})
+
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
